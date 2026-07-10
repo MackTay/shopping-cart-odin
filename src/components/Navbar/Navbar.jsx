80b89ec0cc351053741e1cac7a9c37c4e69152d4
MackTay <191326787+MackTay@users.noreply.github.com>
2026-07-10 15:15:06 +0900

    Add fonts and nav component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import '../../index.css'
 import { Link } from "react-router";
+import styles from './Navbar.module.css'
 // import Cart from '../Cart/Cart.jsx'
 
 function Navbar() {
     return (
         <nav>
-            <Link>Home</Link>
-            <Link>Store</Link>
+            <Link className={styles.home}>Home</Link>
+            <Link className={styles.store}>Store</Link>
+            <Link className={styles.placeholder}>Cart</Link>
             {/* <Cart /> */}
         </nav>
     );
