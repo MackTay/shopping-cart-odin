73755cabb140ec88029790e961add46bafff6711
MackTay <191326787+MackTay@users.noreply.github.com>
2026-07-13 11:31:39 +0900

    Fetch inventory, store state, handle loading/error
    Need access to product info for the shop. Fetch from API and set up as
    Inventory state. Set up loading and error state for loading screen and
    server errors.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import { useState } from 'react'
-import './index.css'
-import Navbar from './components/Navbar/Navbar.jsx'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './index.css';
+import Navbar from './components/Navbar/Navbar.jsx';
 import { Outlet } from "react-router";
 
 function App() {
+  const [inventory, setInventory] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products', {
+      headers: {
+        "User-Agent": "top-practice"
+      }
+  })
+  .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+  })
+  .then((response => setInventory(response)))
+  .catch((error) => setError(error))
+  .finally(() => setLoading(false));;
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
+  console.log(inventory);
   
   return (
     <>
