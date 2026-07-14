e6caf1a8254ca79ae47bdc2288704c4ab7e278b4
MackTay <191326787+MackTay@users.noreply.github.com>
2026-07-14 16:22:23 +0900

    Add hero image and text to homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useOutletContext } from "react-router";
 import { Link } from "react-router";
 import '../../index.css';
+import shopping from '../../assets/shopping.jpg'
 import styles from './HomePage.module.css';
 
 function HomePage() {
@@ -15,16 +16,24 @@ function HomePage() {
     
     return (
         <div className="home">
+            <div className={styles.heroDiv}>
+                <img className={styles.heroImg} src={shopping} alt="" />
+                <div className={styles.heroText}>
+                    <h2>The Products You Need, at Prices You're Kind of Maybe Okay With</h2>
+                    <p>At this shop, we're committed to getting the lowest prices possible on a collection of products that you may not find to have any particular theme.</p>
+                    <p>Also, are you aware of broccoli? We don't sell it here, but it's very nutritious and can be used in a variety of different kinds of cooking. Please give it a try if you haven't!</p>
+                </div>
+            </div>
             <div className={styles.products}>
                 <h2>10s of HOT items to choose from</h2>
                 <div className={styles.gallery1}>
-                    <img src={exInvArr[0]} alt="" />
+                    <img src={exInvArr[0]} alt="example product" />
                     </div>
                 <div className={styles.gallery2}>
-                    <img src={exInvArr[1]} alt="" />
+                    <img src={exInvArr[1]} alt="example product" />
                     </div>
                 <div className={styles.gallery3}>
-                    <img src={exInvArr[2]} alt="" />
+                    <img src={exInvArr[2]} alt="example product" />
                     </div>
                 <Link to='/shop' className={styles.shopLink}>Shop Now!</Link>
             </div>
