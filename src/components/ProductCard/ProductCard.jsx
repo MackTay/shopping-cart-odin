f49d2505324c7f2793d8a9c915cf507bcf9dbae8
MackTay <191326787+MackTay@users.noreply.github.com>
2026-07-29 11:34:16 +0900

    Add input to ProductCard in ShopPage.
    User could not directly enter their preferred number of items to add to
    cart. Add an input that allows the user to directly type a number which
    can then be edited with the increment/decrement buttons.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -38,6 +38,10 @@ function ProductCard({ product }) {
         }
     };
 
+    function inputChange() {
+
+    }
+
     return (
         <div className={styles.card}>
             <img className={styles.cardImg} src={product.image} alt={product.title} />
@@ -50,7 +54,7 @@ function ProductCard({ product }) {
             <div className={styles.btnDiv}>
                 <div className={styles.itemCount}>
                     <button onClick={decrease} className={styles.left}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 16l4-4l-1.4-1.4l-1.6 1.6V8h-2v4.2l-1.6-1.6L8 12zm0 6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"/></svg></button>
-                    <span>{count}</span>
+                    <input onChange={(e) => setCount(Number(e.target.value))} type="number" name="itemCount" id="countDisp" min="0" max="100" value={count} />
                     <button onClick={increase} className={styles.right}><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M11 16h2v-4.2l1.6 1.6L16 12l-4-4l-4 4l1.4 1.4l1.6-1.6zm1 6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8"/></svg></button>
                 </div>
                 <button onClick={() => submit(product)} className={styles.submit}>Add to cart</button>
